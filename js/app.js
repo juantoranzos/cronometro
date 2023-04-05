32b59a56bfe079784ecd2d226390b9199768c117
juantoranzos <toranzos628@gmail.com>
2023-04-05 02:19:42 -0300

    variables del cronometro

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-         
+let pantalla = document.getElementById('pantalla');
+
+let iniciar = document.getElementById('iniciar');
+
+let pausa = document.getElementById('pausa');
+
+let reiniciar = document.getElementById('reiniciar');
+
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let timer;
+
